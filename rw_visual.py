aa40375c22409a12cd4832c78b2e833d27817fc6
Samuel Joshua <samuel.joshua@g.globo>
2024-01-06 09:25:33 -0300

    feat: add random_walk module

--- rw_visual.py
+++ rw_visual.py
@@ -14,8 +14,12 @@ while True:
     # Enfatiza o primeiro e o último ponto
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red',edgecolors='none', s=100)
-    
+
+    # Remove os eixos
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
     plt.show()
+
 
     keep_running = input("Make another walk? (y/n):")
     if keep_running == 'n':
