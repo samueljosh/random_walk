639ced61f35a5bce217f91b06783d1fc2b595124
Samuel Joshua <samuel.joshua@g.globo>
2024-01-08 08:16:17 -0300

    feat: plot settings changed and edges visibility

--- rw_visual.py
+++ rw_visual.py
@@ -5,19 +5,24 @@ from random_walk import RandomWalk
 
 while True:
     # Cria um passeio aleatório e plota os pontos
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
+    # Define o tamanho da janela de plotagem
+    plt.figure(dpi=128,figsize=(12, 8))
+
+    # Plota os pontos e mostra o gráfico
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-                edgecolors='none', s= 15)
+                edgecolors='none', s=1)
     # Enfatiza o primeiro e o último ponto
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red',edgecolors='none', s=100)
 
     # Remove os eixos
-    plt.axes().get_xaxis().set_visible(False)
-    plt.axes().get_yaxis().set_visible(False)
+    #plt.axes().get_xaxis().set_visible(False)
+    #plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
 
